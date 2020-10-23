ccf77e6cb8fc2d5181cc68e6df249428d05bb610
Javiuty <javiiescuadra@gmail.com>
2020-10-23 12:20:24 +0200

    working on 'Creating new account'

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NuevaCuenta = () => {
-  return <h1>Desde NuevaCuenta</h1>;
+  // State para iniciar sesión
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+  });
+
+  // extraer de usuario
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // cuando el usuario quiere iniciar sesion
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // Validar que no haya campos vacios
+
+    // Pasarlo al action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+        <Link to={"/nueva-cuenta"} className="enlace-cuenta">
+          Obtener Cuenta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NuevaCuenta;
